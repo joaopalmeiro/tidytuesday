d7d07dca274dd2fb22d19d7da7062a906603fccd
João Palmeiro <joaommpalmeiro@gmail.com>
2020-01-16 00:46:20 +0000

    Updated: Logic; Added: First version of the plot.

--- passwords.R
+++ passwords.R
@@ -21,17 +21,100 @@ passwordswork
 length(unique(passwordswork$password)) == nrow(passwordswork)
 
 # Levenshtein distance
-levenshtein.distance <- function(df, col, word, topn) {
+levenshtein.distance <- function(df, col, word) {
   distances <- mapply(adist,
                       df[[col]],
                       word,
                       MoreArgs = list(partial = FALSE, ignore.case = FALSE))
   
-  distancesdf <- bind_cols(password = df[[col]], distance = distances)
+  passwordcol <- factor(df[[col]], levels = df[[col]])
+  
+  distancesdf <-
+    bind_cols(password = passwordcol, distance = distances)
   
   topndf <- arrange(distancesdf, desc(distance))
   
-  return(topndf[1:topn, ])
+  topndf$password <-
+    factor(topndf$password, levels = topndf$password)
+  
+  return(topndf)
 }
 
-levenshtein.distance(passwordswork, "password", "password", 10)
+password.test <-
+  levenshtein.distance(passwordswork, "password", "password")
+print(password.test, n = 10)
+
+count.equal.last.distance <- function(df, col, topn) {
+  colref <- df[[col]]
+  distanceref <- colref[topn]
+  
+  counttop <- sum(colref[1:topn] == distanceref)
+  counttotal <- sum(colref == distanceref)
+  
+  return(counttotal - counttop)
+}
+
+count.equal.last.distance(password.test, "distance", 10)
+
+clean.style <- function() {
+  fonttitle <- "Roboto"
+  fontsubtitle <- "Roboto Thin"
+  
+  ggplot2::theme(
+    plot.title = ggplot2::element_text(family = fonttitle,
+                                       color = "#2F2F2F"),
+    plot.subtitle = ggplot2::element_text(
+      family = fontsubtitle,
+      size = 8,
+      color = "#2F2F2F"
+    ),
+    axis.title = ggplot2::element_text(
+      family = fontsubtitle,
+      size = 8,
+      color = "#2F2F2F"
+    ),
+    axis.text = ggplot2::element_text(family = fonttitle,
+                                      color = "#2F2F2F"),
+    panel.grid.major.x = ggplot2::element_blank(),
+    panel.grid.minor.y = ggplot2::element_blank()
+  )
+}
+
+get.plot <- function(df, col.pass, col.distance, word, topn) {
+  df.pre.plot <- levenshtein.distance(df, col.pass, word)
+  count <-
+    count.equal.last.distance(df.pre.plot, col.distance, topn)
+  
+  df.to.plot <- df.pre.plot[1:topn,]
+  max.distance <- max(df.to.plot[[col.distance]])
+  
+  bar.plot <-
+    ggplot(df.to.plot, aes_string(x = col.pass, y = col.distance)) +
+    geom_bar(stat = "identity",
+             fill = "#DFCDC3") +
+    scale_y_continuous(
+      expand = c(0, 0),
+      limits = c(0, max.distance),
+      breaks = seq(0, max.distance, 1)
+    ) +
+    list(theme_minimal() + clean.style()) +
+    geom_hline(yintercept = 0,
+               size = 0.5,
+               colour = "#2F2F2F") +
+    geom_hline(yintercept = max.distance,
+               size = 0.5,
+               colour = "#2F2F2F") +
+    labs(
+      title = "",
+      subtitle = "",
+      x = "Password",
+      y = "Levenshtein distance"
+    )
+  
+  return(bar.plot)
+}
+
+get.plot(passwordswork, "password", "distance", "password", 10)
+
+setwd("~/tidytuesday")
+ggsave("passwords.png")
