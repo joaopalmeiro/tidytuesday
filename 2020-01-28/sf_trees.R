1c602d872a637e344f218dd645d7c51e6eb99618
João Palmeiro <joaommpalmeiro@gmail.com>
2020-01-28 10:35:30 +0000

    Updated: SF Trees - data wrangling.

--- 2020-01-28/sf_trees.R
+++ 2020-01-28/sf_trees.R
@@ -30,3 +30,42 @@ sf_tree_work <-
     sep = " :: ",
     remove = TRUE
   )
+
+sf_tree_work$species_name <-
+  str_remove(sf_tree_work$species_name, "(\\s?::)")
+sf_tree_work$species_name[sf_tree_work$species_name == ""] <- NA
+
+count_species_common_name <-
+  sf_tree_work %>% count(species_name, sort = FALSE)
+print(count_species_common_name, n = nrow(count_species_common_name))
+
+count_species_scientific_name <-
+  sf_tree_work %>% count(species_name, sort = FALSE)
+print(count_species_scientific_name,
+      n = nrow(count_species_scientific_name))
+
+# Acer buergeranum -> Acer buergerianum
+ids_pre_replacement <- sf_tree_work %>% filter(species_name == "Acer buergeranum")
+ids_pre_replacement <- ids_pre_replacement$tree_id
+ids_pre_replacement
+
+sf_tree_work$species_name <-
+  str_replace(sf_tree_work$species_name, "Acer buergeranum", "Acer buergerianum")
+ids_post_replacement <- sf_tree_work %>% filter(species_name == "Acer buergerianum")
+ids_post_replacement <- ids_post_replacement$tree_id
+ids_post_replacement
+
+sum(ids_pre_replacement == ids_post_replacement) == length(ids_pre_replacement)
+
+# Magnolia grandiflora 'Saint Mary' -> Magnolia grandiflora 'St. Mary'
+ids_pre_replacement <- sf_tree_work %>% filter(species_name == "Magnolia grandiflora 'Saint Mary'")
+ids_pre_replacement <- ids_pre_replacement$tree_id
+ids_pre_replacement
+
+sf_tree_work$species_name <-
+  str_replace(sf_tree_work$species_name, "Magnolia grandiflora 'Saint Mary'", "Magnolia grandiflora 'St. Mary'")
+ids_post_replacement <- sf_tree_work %>% filter(species_name == "Magnolia grandiflora 'St. Mary'")
+ids_post_replacement <- ids_post_replacement$tree_id
+ids_post_replacement
+
+sum(ids_pre_replacement == ids_post_replacement) == length(ids_pre_replacement)
