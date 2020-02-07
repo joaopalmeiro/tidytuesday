1e2abbd9a62ff64dac40a8ee1596a6f6dc4c3c5b
João Palmeiro <joaommpalmeiro@gmail.com>
2020-02-07 01:04:10 +0000

    Added: nfl-stadium-attendance first look.

---
+++ 2020-02-04/nfl-stadium-attendance.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+# Datasets.
+attendance <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/attendance.csv')
+standings <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/standings.csv')
+games <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-02-04/games.csv')
+
+glimpse(attendance)
+
+is.na(attendance) %>% colSums()
+
+count_year <- attendance %>% count(year, sort = TRUE)
+print(count_year, n = nrow(count_year))
+
+cardinals_2000 <- attendance %>% filter(year == 2000 & team_name == "Cardinals")
+
+sum(cardinals_2000$weekly_attendance, na.rm = TRUE) == mean(cardinals_2000$total)
